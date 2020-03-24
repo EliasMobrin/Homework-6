c13000ab60bfc96f622e827065f7ce703495a4ab
Elias Mobrin <l.mobrin@setonhill.edu>
2020-03-24 16:23:39 +0100

    Initial Commit

---
+++ In Class Voting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  In Class Voting
+//
+//  Created by Elias Mobrin on 3/24/20.
+//  Copyright © 2020 Elias Mobrin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
